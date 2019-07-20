d12969a3834f996ddc7c23c5cbea4895fa52efd5
Leandro-Cardoso <leandrolimacardoso@hotmail.com>
2019-07-20 16:18:08 -0300

    Histograma Bruto

--- diagram_generator.js
+++ diagram_generator.js
@@ -8,9 +8,8 @@ class Histogram{
         this.min = this.data[0]
         this.amplitude = this.max - this.min
         this.interval_classes = this.amplitude / this.num_classes
-        this.frequency_classes = this.frequency_classes()
     }
-    frequency_classes(){
+    frequency_array(){
         var frequency = []
         for(var i = 0; i <= this.n - 1; i++){
             var sum = 0
@@ -23,8 +22,29 @@ class Histogram{
         }
         return frequency
     }
+    max_frequency(){
+        var frequency_array = this.frequency_array()
+        frequency_array.sort()
+        return frequency_array[frequency_array.length - 1]
+    }
+    draw(){
+        var position = document.getElementById('diagram')
+        var max_frequency = this.max_frequency()
+        var frequency_array = this.frequency_array()
+        for(var i = 0; i <= this.num_classes - 1; i++){
+            var identity = 'div' + (i + 1) //IDENTITY DIVS
+            position.innerHTML += `<div id="${identity}"></div>`
+            var bar = document.getElementById(identity)
+            bar.style.width = `${100 / this.num_classes}%`
+            bar.style.height = `${100 - (100 * frequency_array[i] / max_frequency)}%`
+            bar.style.backgroundColor = 'rgb(206, 206, 206)'
+            bar.style.display = 'block'
+            bar.style.position = 'absolute'
+            bar.style.left = `${100 / this.num_classes * i}%`
+            bar.innerHTML += frequency_array[i]
+        }
+    }
 }
-//TERMINAR HITOGRAMA
 //------------------------------------------------DRAW DIAGRAMS
 var histogram_x = new Histogram(data_x.data, 5)
-document.getElementById('diagram').innerHTML = histogram_x.frequency_classes
+histogram_x.draw()
